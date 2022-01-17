b288443c0476597a8404c2ec6eae8426eaa000f6
SerhiiPavlichuk <S_Pavlichuk@icloud.com>
2022-01-17 23:46:31 +0200

    added SettingScreen interface Implemented hiding the keyboard when pressing the Return button

--- MagicBall/MagicBall/Modules/SettingViewController/View/SettingViewController.swift
+++ MagicBall/MagicBall/Modules/SettingViewController/View/SettingViewController.swift
@@ -8,6 +8,18 @@
 import Foundation
 import UIKit
 
-class SettingViewController: UIViewController {
+class SettingViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var userInputTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.userInputTextField.delegate = self
+
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
 }
